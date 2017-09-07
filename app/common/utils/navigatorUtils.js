baa47f422f71350042a3b024cc054b276e35e39b
JDEast1029 <852778359@qq.com>
2017-09-07 19:30:20 +0800

    删除console.log

--- app/common/utils/navigatorUtils.js
+++ app/common/utils/navigatorUtils.js
@@ -5,7 +5,6 @@
 export const getKeyForRouteName = (key, routes) => {
 	if (routes && routes instanceof Array) {
 		let route = routes.find((route) => route.routeName === key);
-		console.log(route.key)
 
 		return route.key
 	}
